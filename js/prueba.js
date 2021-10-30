24d84aca4905186090b202217e5e424e7c024c49
Hiroshi Taco <hiroshitaco@gmail.com>
2021-10-29 23:01:36 -0500

    actualizadoUWU

--- js/prueba.js
+++ js/prueba.js
@@ -1,73 +1,15 @@
-const readmore1 = document.querySelector('.read-more-btn1');
-const text1 = document.querySelector('.text1');
+var readmore = [];
+var text = [];
+for(let i = 1; i <=9; i++){
+  readmore[i]=document.querySelector('.read-more-btn'+i);
+  text[i] = document.querySelector('.text'+i);
+  readmore[i].addEventListener('click', (e)=>{
+    text[i].classList.toggle('show-more'+i);
+    if(readmore[i].innerText ==='Leer menos'){
+      readmore[i].innerText = ' Leer más';
+    }else{
+      readmore[i].innerText = 'Leer menos';
+    }
+  })
+}
 
-readmore1.addEventListener('click', (e)=>{
-  text1.classList.toggle('show-more1');
-  if(readmore1.innerText ==='Leer menos'){
-    readmore1.innerText = ' Leer más';
-  }else{
-    readmore1.innerText = 'Leer menos';
-  }
-})
-
-const readmore2 = document.querySelector('.read-more-btn2');
-const text2 = document.querySelector('.text2');
-
-readmore2.addEventListener('click', (e)=>{
-  text2.classList.toggle('show-more2');
-  if(readmore2.innerText ==='Leer menos'){
-    readmore2.innerText = ' Leer más';
-  }else{
-    readmore2.innerText = 'Leer menos';
-  }
-})
-
-const readmore3 = document.querySelector('.read-more-btn3');
-const text3 = document.querySelector('.text3');
-
-readmore3.addEventListener('click', (e)=>{
-  text3.classList.toggle('show-more3');
-  if(readmore3.innerText ==='Leer menos'){
-    readmore3.innerText = ' Leer más';
-  }else{
-    readmore3.innerText = 'Leer menos';
-  }
-})
-
-
-const readmore4 = document.querySelector('.read-more-btn4');
-const text4 = document.querySelector('.text4');
-
-readmore4.addEventListener('click', (e)=>{
-  text4.classList.toggle('show-more4');
-  if(readmore4.innerText ==='Leer menos'){
-    readmore4.innerText = ' Leer más';
-  }else{
-    readmore4.innerText = 'Leer menos';
-  }
-})
-
-
-const readmore5 = document.querySelector('.read-more-btn5');
-const text5 = document.querySelector('.text5');
-
-readmore5.addEventListener('click', (e)=>{
-  text5.classList.toggle('show-more5');
-  if(readmore5.innerText ==='Leer menos'){
-    readmore5.innerText = ' Leer más';
-  }else{
-    readmore5.innerText = 'Leer menos';
-  }
-})
-
-const readmore6 = document.querySelector('.read-more-btn6');
-const text6 = document.querySelector('.text6');
-
-readmore6.addEventListener('click', (e)=>{
-  text6.classList.toggle('show-more6');
-  if(readmore6.innerText ==='Leer menos'){
-    readmore6.innerText = ' Leer más';
-  }else{
-    readmore6.innerText = 'Leer menos';
-  }
-})
